c333416a2101a8575799668db841b892c91c38f9
Srilekha Ganesan <srilekhaganesan@MacBook-Pro.local>
2021-10-24 22:58:28 +0100

    basic command execution

--- src/marsrover_start.js
+++ src/marsrover_start.js
@@ -1,8 +1,33 @@
+//import {determineDir,determineLoc} from './determine.mjs';
+//determineDir
+const {determineDir,determineLoc} = require('./determine.js');
+//const {determineLoc} = require('./determine.js');
 
 const commandExec = command => {
-    return '1 3 N'
 
+
+    const [maxX,maxY] = command[0].split(' ')
+   // console.log(maxX)
+    //console.log(maxY)
+
+    let [locX,locY,dir] = command[1].split(' ')
+   // console.log(locX,locY,dir)
+    
+    let cmd = command[2].split('')
+    console.log(cmd) 
+    let DD =''
+    let DE =''
+
+    cmd.forEach((cmdi) => {
+        if (cmdi == 'L' || cmdi == 'R') { 
+            dir=determineDir(dir,cmdi)
+        }
+        if (cmdi == 'M'){
+            DD= (determineLoc(maxX,maxY,parseInt(locX),parseInt(locY),dir))
+        }
+    })
+    console.log(DD)
+    return (DD+' '+dir)
 }
 
-//export default commandExec;
 module.exports = commandExec
